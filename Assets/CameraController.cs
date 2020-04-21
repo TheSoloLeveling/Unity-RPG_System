30225926ebdc5bee7f66c48d833cd9af9756cd98
TheSoloLeveling <62574566+TheSoloLeveling@users.noreply.github.com>
2020-04-21 19:05:35 +0000

    Add a RPG character and create 2 scripts for it PlayerMotor and PlayerController + script for Camera

---
+++ Assets/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    public float pitch = 3.2f;
+    private float currentZomm = 10f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = target.position - (offset * currentZomm);
+        transform.LookAt(target.position + Vector3.up * pitch);
+    }
+}
---
+++ Assets/Sripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    private Camera cam;
+    private Animator anim;
+
+    PlayerMotor motor;
+    void Start()
+    {
+        cam = Camera.main;
+        motor = GetComponent<PlayerMotor>();
+        anim = GetComponent<Animator>();
+
+    }
+
+    
+    void FixedUpdate()
+    {
+        
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                motor.movePlayer(hit.point);
+                anim.speed = motor.agent.speed;
+            }
+        }
+        
+    }
+}
