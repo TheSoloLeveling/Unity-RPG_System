fb0eb6af6913b17abede749ad940d45c45a7adca
TheSoloLeveling <62574566+TheSoloLeveling@users.noreply.github.com>
2020-04-21 19:31:20 +0000

    Update CameraController script, rotate the camera with keys and zoom with mouse scroll

--- Assets/Sripts/CameraController.cs
+++ Assets/Sripts/CameraController.cs
@@ -5,13 +5,30 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public Transform target;
+
     public Vector3 offset;
+    public float zommSpeed = 4f;
+    public float minZoom = 5f;
+    public float maxZoom = 15f;
+
+    public float yawSpeed = 100f;
     public float pitch = 3.2f;
     private float currentZomm = 10f;
+
+    private float yawInput = 0f;
+   
 
     void Start()
     {
         
+    }
+
+    private void Update()
+    {
+        currentZomm -= Input.GetAxis("Mouse ScrollWheel") * zommSpeed;
+        currentZomm = Mathf.Clamp(currentZomm, minZoom, maxZoom);
+
+        yawInput += Input.GetAxis("Horizontal") * yawSpeed * Time.deltaTime;
     }
 
     // Update is called once per frame
@@ -19,5 +36,7 @@ public class CameraController : MonoBehaviour
     {
         transform.position = target.position - (offset * currentZomm);
         transform.LookAt(target.position + Vector3.up * pitch);
+
+        transform.RotateAround(target.position, Vector3.up, yawInput);
     }
 }
