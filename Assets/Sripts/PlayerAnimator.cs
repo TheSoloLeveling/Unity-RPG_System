a644a80c900dab0e4f44a39e3c4e1dd139eb74da
TheSoloLeveling <62574566+TheSoloLeveling@users.noreply.github.com>
2020-04-21 21:22:51 +0000

    Create Player animation script and add animations to the animator

---
+++ Assets/Sripts/PlayerAnimator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private Animator anim;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speed = agent.velocity.magnitude / agent.speed;
+        anim.SetFloat("Speed", speed, 0.1f, Time.deltaTime);
+
+    }
+}
