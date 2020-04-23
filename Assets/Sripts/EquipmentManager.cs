5e20e9a5e0855acecc97865608766602620ef368
TheSoloLeveling <62574566+TheSoloLeveling@users.noreply.github.com>
2020-04-23 20:44:50 +0000

    Adding Equipment Scriptable Object with Adding EquipmentManager script

---
+++ Assets/Sripts/EquipmentManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EquipmentManager : MonoBehaviour
+{
+    #region Singleton
+    public static EquipmentManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    #endregion
+
+    Equipment[] currentEquipment;
+
+    private void Start()
+    {
+        int numSlots = System.Enum.GetNames(typeof(Equipment.EquipmentSlot)).Length;   // get the amount of elements in an enum
+        currentEquipment = new Equipment[numSlots];
+    }
+
+    public void Equip (Equipment newItem)
+    {
+        int slotIndex = (int)newItem.equipmentSlot;  // (int) will take the number (index) of the correspandant equipment inside the enum
+        currentEquipment[slotIndex] = newItem;     
+    }
+
+}
