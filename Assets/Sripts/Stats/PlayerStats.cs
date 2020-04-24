e0298a1fc86e44c60019a5795735972048ea260b
TheSoloLeveling <62574566+TheSoloLeveling@users.noreply.github.com>
2020-04-24 18:22:54 +0000

    Add A stat and character stats System adding 3 new scripts to manange the system

---
+++ Assets/Sripts/Stats/PlayerStats.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : CharacterStats
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        EquipmentManager.instance.onEquipmentChanged += OnEquipmentChanged;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnEquipmentChanged(Equipment newItem, Equipment olditem)
+    {
+
+    }
+}
---
+++ Assets/Sripts/Stats/Stat.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Stat
+{
+    [SerializeField]
+    private int baseValue;
+
+    private List<int> modifiers = new List<int>();
+
+    public int GetValue()
+    {
+        return baseValue;
+    }
+   
+}
