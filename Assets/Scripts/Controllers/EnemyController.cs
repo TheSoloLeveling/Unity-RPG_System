8ddf05febba22928e81ce687f106f712b789a2c6
TheSoloLeveling <62574566+TheSoloLeveling@users.noreply.github.com>
2020-04-25 23:18:05 +0000

    Adding an enemy prefab to the scene with creating and associating to it a controller and animtor scripts and also an empty enemy stats script

--- Assets/Scripts/Controllers/EnemyController.cs
+++ Assets/Scripts/Controllers/EnemyController.cs
@@ -6,18 +6,37 @@ using UnityEngine.AI;
 public class EnemyController : MonoBehaviour
 {
     public float lookRadius = 10f;
-    public Transform target;
+    private Transform target;
 
     private NavMeshAgent agent;
 
     void Start()
     {
+        target = PlayerManager.instance.player.transform;
         agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if (distance <= lookRadius)
+        {
+            agent.SetDestination(target.position);
+
+            if (distance <= agent.stoppingDistance)
+            {
+
+                FaceTarget();
+            }
+        }
+    }
+
+    void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
     }
 }
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : Interactable
+{
+    PlayerManager playerManager;
+
+    public override void Interact()
+    {
+        base.Interact();
+
+    }
+
+}
