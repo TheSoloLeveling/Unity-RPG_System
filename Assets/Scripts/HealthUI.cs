215232416eb408bbdf65db1fc21689eea4e4421e
TheSoloLeveling <62574566+TheSoloLeveling@users.noreply.github.com>
2020-04-27 00:22:06 +0000

    Import new Health bar assest and set it to the canvas in the scene with adding a Health Ui script manage the health bar

---
+++ Assets/Scripts/HealthUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthUI : MonoBehaviour
+{
+    public GameObject uiPrefab;
+    public Transform target;
+
+    Transform ui;
+    Image healthSlider;
+    Transform cam;
+
+    void Start()
+    {
+        cam = Camera.main.transform;
+
+        foreach (Canvas c in FindObjectsOfType<Canvas>())
+        {
+            if (c.renderMode == RenderMode.WorldSpace)
+            {
+                ui = Instantiate(uiPrefab, c.transform).transform;
+                break;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        ui.position = target.position;
+        ui.forward = -cam.forward;
+    }
+}
