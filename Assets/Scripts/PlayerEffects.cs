497ca3c2c756dca4e66c363455d82f03c32caf89
TheSoloLeveling <62574566+TheSoloLeveling@users.noreply.github.com>
2020-04-27 13:58:26 +0000

    scene Update and Add new PArticle effects with scripts PlayerEffects managing it

---
+++ Assets/Scripts/PlayerEffects.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEffects : MonoBehaviour
+{
+    [SerializeField] ParticleSystem collectParticle = null;
+    
+
+    private void Start()
+    {
+        collectParticle.Stop();
+        collectParticle.gameObject.SetActive(false);
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Collect();
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            DisableAll();
+        }
+    }
+
+    public void Collect()
+    {
+        collectParticle.Play();
+        collectParticle.gameObject.SetActive(true);
+    }
+
+    public void DisableAll()
+    {
+        collectParticle.Stop();
+        collectParticle.gameObject.SetActive(false);
+    }
+
+    
+}
